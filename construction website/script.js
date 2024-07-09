12a11d1e6844c0e07fe7828b34d630b639e9f2a9
anitakevale <anitakevale10@gmail.com>
2024-07-09 09:26:04 -0700

    Delete construction website directory

--- construction website/script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menu = document.querySelector('.menu');
-
-hamburgerMenu.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
